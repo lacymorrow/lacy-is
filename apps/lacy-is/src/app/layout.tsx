67aee6367aa8fdbf74e47c703f8498d85c41debe
Lacy Morrow <me@lacymorrow.com>
2024-07-30 14:37:13 -0400

    save

--- apps/lacy-is/src/app/layout.tsx
+++ apps/lacy-is/src/app/layout.tsx
@@ -4,58 +4,58 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata, Viewport } from "next";
 
-import "~/styles/globals.css";
+import "@/styles/globals.css";
 
+import { env } from "@/env";
 import { cn } from "@/lib/utils";
-import { env } from "~/env";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    env.VERCEL_ENV === "production"
-      ? `https://${env?.VERCEL_URL || "lacy.is"}`
-      : "http://localhost:3000",
-  ),
-  title: "lacy.is",
-  description: "be a better person.",
-  openGraph: {
-    title: "lacy.is",
-    description: "be a better person.",
-    url: "https://lacy.is",
-    siteName: "lacy.is",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@lacy_is",
-    creator: "@lacy_is",
-  },
+	metadataBase: new URL(
+		env.VERCEL_ENV === "production"
+			? `https://${env?.VERCEL_URL || "lacy.is"}`
+			: "http://localhost:3000",
+	),
+	title: "lacy.is",
+	description: "be a better person.",
+	openGraph: {
+		title: "lacy.is",
+		description: "be a better person.",
+		url: "https://lacy.is",
+		siteName: "lacy.is",
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: "@lacy_is",
+		creator: "@lacy_is",
+	},
 };
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans text-foreground antialiased",
-          GeistSans.variable,
-          GeistMono.variable,
-        )}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {props.children}
-          <div className="absolute bottom-4 right-4">
-            <ThemeToggle />
-          </div>
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans text-foreground antialiased",
+					GeistSans.variable,
+					GeistMono.variable,
+				)}
+			>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{props.children}
+					<div className="absolute bottom-4 right-4">
+						<ThemeToggle />
+					</div>
 
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+					<Toaster />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- apps/lacy-is/src/middleware.disable.ts
+++ apps/lacy-is/src/middleware.disable.ts
@@ -1,22 +1,22 @@
+import { precomputeFlags } from '@/feature-flags';
 import { unstable_precompute as precompute } from '@vercel/flags/next';
 import { NextResponse, type NextRequest } from 'next/server';
-import { precomputeFlags } from '~/feature-flags';
 
 // Note that we're running this middleware for / only, but
 // you could extend it to further pages you're experimenting on
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
 
 export async function middleware(request: NextRequest) {
-  // precompute returns a string encoding each flag's returned value
-  const code = await precompute(precomputeFlags);
+	// precompute returns a string encoding each flag's returned value
+	const code = await precompute(precomputeFlags);
 
-  // rewrites the request to include the precomputed code for this flag combination
-  const nextUrl = new URL(
-    `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
-    request.url,
-  );
+	// rewrites the request to include the precomputed code for this flag combination
+	const nextUrl = new URL(
+		`/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
+		request.url,
+	);
 
-  return NextResponse.rewrite(nextUrl, { request });
+	return NextResponse.rewrite(nextUrl, { request });
 }
